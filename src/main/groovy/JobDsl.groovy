4931bf9549e82b62bb2044af49d9d27e1b4a4794
nlabrot <nithril@gmail.com>
2015-04-23 23:37:24 +0200

    Pipeline view

--- src/main/groovy/JobDsl.groovy
+++ src/main/groovy/JobDsl.groovy
@@ -3,8 +3,6 @@ import groovy.json.JsonSlurper
 def projects = new JsonSlurper().parseText(readFileFromWorkspace("src/main/groovy/project.json"))
 
 
-
-
 def projectScm = { owner, project ->
     delegate = owner
     scm {
@@ -16,51 +14,36 @@ def projectScm = { owner, project ->
             createTag false
         }
     }
-
 }
 
 projects.each { project ->
 
-
+    //Define projects name
     def compileProjectName = "${project.name} - Compile"
     def testProjectName = "${project.name} - Test"
     def packageProjectName = "${project.name} - Package"
 
-    buildPipelineView("${project.name}") {
-        selectedJob compileProjectName
-        displayedBuilds 3
-        showPipelineParameters true
-        showPipelineParametersInHeaders true
-        showPipelineDefinitionHeader true
-
-    }
-
-
+    //Compile Job
     mavenJob(compileProjectName) {
-
         projectScm(delegate, project)
-
         goals "compile"
         publishers {
             downstream testProjectName
         }
     }
 
-
+    //Test Job
     mavenJob(testProjectName) {
-
         projectScm(delegate, project)
-
         goals "test"
         publishers {
             downstream packageProjectName
         }
     }
 
+    //Package Job
     freeStyleJob(packageProjectName) {
-
         projectScm(delegate, project)
-
         steps {
             maven {
                 goals "package"
@@ -68,5 +51,15 @@ projects.each { project ->
             }
             shell "cp submodule/target/*.jar /dev/null"
         }
+    }
+
+    //Create the Pipeline view
+    buildPipelineView("${project.name}") {
+        selectedJob compileProjectName
+        displayedBuilds 3
+        showPipelineParameters true
+        showPipelineParametersInHeaders true
+        showPipelineDefinitionHeader true
+
     }
 }
